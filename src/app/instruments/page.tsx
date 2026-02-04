7883604cffac4a7dd1c27d37487ee1eeb99b2518
Jurris Leuwe Tolosa <jleuwe@Power-Rangers.local>
2026-02-05 01:17:15 +0800

    fix supabase connection

--- src/app/instruments/page.tsx
+++ src/app/instruments/page.tsx
@@ -1,17 +1,14 @@
-
 import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
 
 export default async function Page() {
-    const cookieStore = await cookies()
-    const supabase = createClient(cookieStore)
+    const supabase = await createClient()
 
     const { data: todos } = await supabase.from('todos').select()
 
     return (
         <ul>
             {todos?.map((todo) => (
-                <li>{todo}</li>
+                <li key={todo.id}>{todo.title ?? JSON.stringify(todo)}</li>
             ))}
         </ul>
     )
